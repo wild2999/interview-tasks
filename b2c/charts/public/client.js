0fc518051074ad52cc9a70af10287051b1979801
wild999 <wild2999@mail.ru>
2016-05-27 07:17:33 +0300

    master added module system

--- b2c/charts/public/client.js
+++ b2c/charts/public/client.js
@@ -1,86 +1,114 @@
-var GRAPH = (function () {
-
 /**
- *
  * @author Igor Lishchuk
  *
  *
- * Creates an instance of Canvas.
- *
- * @constructor
- * @this {Canvas}
- * @param {String} id The id of the canvas
- * @param {Number} w The width of the canvas
- * @param {Number} h The height of the canvas
  */
 
-function Canvas(id, w, h) {
 
-    var canvas = document.createElement('canvas'),
-        width = w,
-        height = h;
+var graph = (function () {
 
-    canvas.id = id;
-    canvas.width = width;
-    canvas.height = height;
-
+    var graph = {};
 
     /**
-     * Get canvas element from the DOM.
      *
-     * @return {DOMElement} The DOM element
+     * Creates an instance of Canvas.
+     *
+     * @constructor
+     * @this {Canvas}
+     * @param {String} id The id of the canvas
+     * @param {Number} w The width of the canvas
+     * @param {Number} h The height of the canvas
      */
-    this.getCanvas = function () {
-        return canvas;
+
+    graph.Canvas = function (id, w, h) {
+
+        var canvas = document.createElement('canvas'),
+            width = w,
+            height = h;
+
+        canvas.id = id;
+        canvas.width = width;
+        canvas.height = height;
+
+
+        /**
+         * Get canvas element from the DOM.
+         *
+         * @return {DOMElement} The DOM element
+         */
+        this.getCanvas = function () {
+            return canvas;
+        };
+
+        /**
+         * Get the width of the canvas
+         *
+         * @returns {Number} The width of the canvas
+         */
+        this.getWidth = function () {
+            return width;
+        };
+
+        /**
+         * Get the height of the canvas
+         *
+         * @returns {Number} The height of the canvas
+         */
+        this.getHeight = function () {
+            return height;
+        };
+
+        document.body.appendChild(canvas);
+
     };
 
     /**
-     * Get the width of the canvas
      *
-     * @returns {Number} The width of the canvas
+     * Creates an instance of Brush.
+     *
+     * @constructor
+     * @this {Brush}
+     * @param {Function} canvas The DOM element
+     * @param {Function} width The width of the canvas
+     * @param {Function} height The height of the canvas
      */
-    this.getWidth = function () {
-        return width;
+    graph.Brush = function (canvas, width, height) {
+        this.canvas = canvas;
+        this.width = width;
+        this.height = height;
     };
 
     /**
-     * Get the height of the canvas
      *
-     * @returns {Number} The height of the canvas
+     * Inherit values and methods ​​from the class rabbit animal class
+     *
+     * @type {Canvas}
      */
-    this.getHeight = function () {
-        return height;
-    };
-}
+    graph.Brush.prototype = Object.create(graph.Canvas.prototype);
+    /**
+     * Write constructor clearly
+     *
+     * @type {Brush}
+     */
+    graph.Brush.prototype.constructor = graph.Brush;
 
-/**
- *
- * Creates an instance of Brush.
- *
- * @constructor
- * @this {Brush}
- * @param {Function} canvas The DOM element
- * @param {Function} width The width of the canvas
- * @param {Function} height The height of the canvas
- */
-function Brush(canvas, width, height) {
     /**
      * Draw a graph
      *
      * @param array Array with points
      */
-    /** @private */ this._draw = function (array) {
+    graph.Brush.prototype.draw = function (array) {
         var i;
 
-        var axes = {}, ctx = canvas.getContext("2d");
-        axes.y0 = .5 * height;
+        var axes = {}, ctx = this.canvas.getContext("2d");
+        axes.y0 = .5 * this.height;
 
         ctx.beginPath();
         ctx.lineWidth = 1;
         ctx.strokeStyle = '#000';
 
         for (i = 0; i < array.length; i += 1) {
-            var x = i * width / 100;
+            var x = i * this.width / 100;
             var y = array[i] + axes.y0;
 
 
@@ -89,82 +117,81 @@ function Brush(canvas, width, height) {
         ctx.stroke();
     };
 
-    document.body.appendChild(canvas);
-
-}
-
-/**
- *
- * Creates an instance of Coord.
- *
- * @constructor
- * @this {Coord}
- * @param url The url for ajax-request
- */
-
-function Coord(url) {
     /**
-     * Do ajax request using promise
      *
-     * @return {Promise}
+     * Creates an instance of Coord.
+     *
+     * @constructor
+     * @this {Coord}
+     * @param url The url for ajax-request
      */
-    var ajax = function () {
-        return new Promise(function (resolve, reject) {
 
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', url, true);
+    graph.Coord = function (url) {
+        /**
+         * Do ajax request using promise
+         *
+         * @return {Promise}
+         */
+        var ajax = function () {
+            return new Promise(function (resolve, reject) {
 
-            xhr.onload = function () {
-                if (this.status == 200) {
-                    resolve(JSON.parse(this.response));
-                } else {
-                    var error = new Error(this.statusText);
-                    error.code = this.status;
-                    reject(error);
-                }
-            };
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', url, true);
 
-            xhr.onerror = function () {
-                reject(new Error("Network Error"));
-            };
+                xhr.onload = function () {
+                    if (this.status == 200) {
+                        resolve(JSON.parse(this.response));
+                    } else {
+                        var error = new Error(this.statusText);
+                        error.code = this.status;
+                        reject(error);
+                    }
+                };
 
-            xhr.send();
+                xhr.onerror = function () {
+                    reject(new Error("Network Error"));
+                };
+
+                xhr.send();
+            });
+        };
+
+        /**
+         *
+         * @returns {ajax}
+         * @private
+         */
+        this._getAjaxFunc = function () {
+            return ajax;
+        };
+    };
+
+    /**
+     *
+     * @type {Canvas}
+     */
+    var canvas = new graph.Canvas('graph', 900, 500);
+
+    /**
+     *
+     * @type {Brush}
+     */
+    var brush = new graph.Brush(canvas.getCanvas(), canvas.getWidth(), canvas.getHeight());
+
+    /**
+     *
+     * @type {Coord}
+     */
+    var coord = new graph.Coord('/api/v1/points');
+
+    graph.init = function () {
+        (coord._getAjaxFunc())().then(function (response) {
+            brush.draw(response);
+        }, function (error) {
+            return console.log("Rejected: " + error);
         });
     };
 
-    /**
-     *
-     * @returns {ajax}
-     * @private
-     */
-    this._getAjaxFunc = function () {
-        return ajax;
-    };
-}
-
-/**
- *
- * @type {Canvas}
- */
-var canvas = new Canvas('graph', 900, 500);
-
-/**
- *
- * @type {Brush}
- */
-var brush = new Brush(canvas.getCanvas(), canvas.getWidth(), canvas.getHeight());
-
-/**
- *
- * @type {Coord}
- */
-var coord = new Coord('/api/v1/points');
-
-(coord._getAjaxFunc())().then(function (response) {
-    brush._draw(response);
-}, function (error) {
-    return console.log("Rejected: " + error);
-});
-
-
+    graph.init();
 })();
+
